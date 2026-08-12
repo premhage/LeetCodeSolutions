7ea80e9e64e10a10583d15370191245b614b68e6
Prem Hage <premhage8@gmail.com>
2026-08-12 23:27:28 +0530

    Time: 2 ms (92.05%), Space: 47.5 MB (93.84%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length==1){
+            return;
+        }
+        int j=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+    }
+}
