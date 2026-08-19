e2160cd69de5552af4fff1e741b4fdfd9a7563aa
Prem Hage <premhage8@gmail.com>
2026-08-19 21:27:23 +0530

    Time: 1 ms (99.98%), Space: 42.7 MB (46.25%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        int n=x;
+        int rev=0;
+        while(n!=0){
+            int digit=n%10;
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rev=rev*10+digit;
+            
+            n=n/10;
+        }
+        return rev;
+    }
+}
